2b466b647ace48a591fdbe0d7e82535941fc4fbe
Spencer Coleman <90425241+spencercolman@users.noreply.github.com>
2021-10-23 23:30:01 +0000

    indent error

--- main.py
+++ main.py
@@ -71,4 +71,29 @@ elif time_now > 12:
 elif time_now > 6:
    user_name = input('Good evening, stranger. I am Chatbot. What is your name? ')
    user_input = input(f'Hello, {user_name}. Happy {calendar.day_name[today_date.weekday()]}! ')
+   study_ans = str.lower(input('Ready to study? '))
+   if study_ans == 'yes':
+    subject_ans = str.lower(input('What subject do you want to study today {user_name} ? '))
+   else:
+    print('Okay... see you next time!')
+    sys.exit()
+
+
+   while True:
+    if subject_ans == 'anatomy':
+     print('hello world')
+     break
+    elif subject_ans == 'chemistry':
+     print('hello world 2')
+     break
+    elif subject_ans == 'epidemiology':
+     print('hello world 3')
+     break
+    else:
+     sec_try = str.lower(input("Sorry, I don't support that yet.. would you like to pick another subject? "))
+    if sec_try == 'yes':
+     subject_ans = str.lower(input(f'What subject do you want to study today {user_name} ? '))
+   else:
+    print('Okay... see you next time!')
+    sys.exit()
 
